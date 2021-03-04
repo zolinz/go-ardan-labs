73c0ac89c51ecd9bb13132a460dd35d3d07434ee
Zoltan Kovacs <>
2021-03-05 07:51:30 +1100

    updated with make and docker

--- language/arrays/arr_example1.go
+++ language/arrays/arr_example1.go
@@ -3,9 +3,12 @@ package main
 
 func main(){
 
-	myarrary := [5]int{10, 20, 30, 40, 50}
+	myarrary := [6]int{10, 20, 30, 40, 50,60}
 	
 	for  i := 0; i< len(myarrary); i++{
 		println(myarrary[i])
 	}
+
+
+	for 
 }
